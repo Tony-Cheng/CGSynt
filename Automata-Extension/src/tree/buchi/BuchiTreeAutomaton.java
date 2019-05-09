e566df6109f3fbce41fabe4b6663475294c3aa75
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-05-09 16:24:56 -0400

    added states and transitions for the Buchi tree automaton.

--- Automata-Extension/src/tree/buchi/BuchiTreeAutomaton.java
+++ Automata-Extension/src/tree/buchi/BuchiTreeAutomaton.java
@@ -1,5 +1,46 @@
 package tree.buchi;
 
-public class BuchiTreeAutomaton {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import de.uni_freiburg.informatik.ultimate.automata.tree.IRankedLetter;
+import de.uni_freiburg.informatik.ultimate.automata.tree.ITreeAutomatonBU;
+import de.uni_freiburg.informatik.ultimate.automata.tree.TreeAutomatonRule;
+
+/**
+ * 
+ * @author
+ *
+ * @param <LETTER>
+ * @param <STATE>
+ */
+public class BuchiTreeAutomaton
+<LETTER extends IRankedLetter, STATE> implements IBuchiTreeAutomaton<LETTER, STATE> {
+	private final Set<LETTER> mAlphabet;
+	private final Map<List<STATE>, Map<LETTER, Collection<TreeAutomatonRule<LETTER, STATE>>>> mChildrenMap;
+	private final Set<STATE> mFinalStates;
+	private final Map<LETTER, Collection<TreeAutomatonRule<LETTER, STATE>>> mLettersMap;
+	private final Map<STATE, Map<LETTER, Collection<TreeAutomatonRule<LETTER, STATE>>>> mParentsMap;
+	private final Set<TreeAutomatonRule<LETTER, STATE>> mRules;
+	private final Map<STATE, Collection<TreeAutomatonRule<LETTER, STATE>>> mSourceMap;
+	private final Set<STATE> mStates;
+	
+	/**
+	 * Create a BuchiTreeAutomaton.
+	 */
+	public BuchiTreeAutomaton() {
+		mChildrenMap = new HashMap<>();
+		mParentsMap = new HashMap<>();
+		mAlphabet = new HashSet<>();
+		mLettersMap = new HashMap<>();
+		mSourceMap = new HashMap<>();
+		mRules = new HashSet<>();
+		mFinalStates = new HashSet<>();
+		mStates = new HashSet<>();
+	}
+	
 }
