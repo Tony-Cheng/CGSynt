8efdfb63fef2c21f0016b5a558c7bea0785340ec
benweisz <ben.weisz@mail.utoronto.ca>
2019-05-09 16:16:13 -0400

    Added method declarations to interface

--- Automata-Extension/src/tree/buchi/IBuchiTreeAutomaton.java
+++ Automata-Extension/src/tree/buchi/IBuchiTreeAutomaton.java
@@ -7,13 +7,15 @@ import de.uni_freiburg.informatik.ultimate.automata.tree.IRankedLetter;
  * @author weiszben
  *
  * @param <LETTER>
- * 			is the type of alphabet
+ * 			is the type of alphabet.
  * @param <STATE>
- * 			is the type of state
+ * 			is the type of states.
  */
 public interface IBuchiTreeAutomaton<LETTER extends IRankedLetter, STATE> extends IAutomaton<LETTER, STATE>{
 	
 	
 	void addRule(final BuchiTreeAutomatonRule<LETTER, STATE> rule);
+	int getAmountOfRules();
 	
+
 }
