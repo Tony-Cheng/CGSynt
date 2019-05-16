d2cfb6f1a001873ab2450cfdc43d856df7eb29eb
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-05-16 12:14:04 -0400

    reimplemented the operation emptiness check.

--- Automata-Extension/src/usra/tree/buchi/operations/EmptinessCheck.java
+++ Automata-Extension/src/usra/tree/buchi/operations/EmptinessCheck.java
@@ -1,10 +1,11 @@
 package usra.tree.buchi.operations;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 import de.uni_freiburg.informatik.ultimate.automata.tree.IRankedLetter;
 import usra.tree.buchi.BuchiTreeAutomaton;
@@ -13,110 +14,143 @@ import usra.tree.buchi.BuchiTreeAutomatonRule;
 public class EmptinessCheck<LETTER extends IRankedLetter, STATE> {
 
 	private BuchiTreeAutomaton<LETTER, STATE> mtree;
-	private Map<STATE, Boolean> isValidTree;
-	private Set<STATE> visited;
-	private Set<STATE> toDelete;
+	private Set<STATE> finalStates;
+	/**
+	 * States that are the roots of good subtree embedded.
+	 */
+	private Set<STATE> goodStates;
+	/**
+	 * Transitions whose destination is a list consists of all acceptance states or
+	 * roots of some good subtree embedded.
+	 */
+	private Stack<BuchiTreeAutomatonRule<LETTER, STATE>> goodTransitions;
 
 	public EmptinessCheck(BuchiTreeAutomaton<LETTER, STATE> mtree) {
 		this.mtree = mtree.mkcpy();
-		isValidTree = new HashMap<>();
-		visited = new HashSet<>();
-		toDelete = new HashSet<>();
+		finalStates = mtree.getFinalStates();
+		goodStates = new HashSet<STATE>();
+		goodTransitions = new Stack<>();
 	}
 
-	/**
-	 * Return true if the tree rooted at curState is a good subtree embedded and
-	 * false otherwise.
-	 * 
-	 * @param starting
-	 *            Whether or not this is the root of the good subtree embedded that
-	 *            we are checking.
-	 * @param curState
-	 *            the current state in the good subtree embedded.
-	 * @return
-	 */
-	private boolean eliminateNode(boolean starting, STATE curState) {
-		if (starting == false && mtree.isFinalState(curState)) {
-			return true;
-		}
-		if (isValidTree.containsKey(curState)) {
-			return isValidTree.get(curState);
-		}
-		if (visited.contains(curState)) {
-			isValidTree.put(curState, false);
-			return false;
-		}
-		Collection<BuchiTreeAutomatonRule<LETTER, STATE>> rules = mtree.getRulesBySource(curState);
-		if (rules == null || rules.isEmpty()) {
-			isValidTree.put(curState, false);
-			toDelete.add(curState);
-			return false;
-		}
-		visited.add(curState);
-		for (BuchiTreeAutomatonRule<LETTER, STATE> rule : rules) {
-			boolean validTree = true;
-			for (STATE dest : rule.getDest()) {
-				if (!eliminateNode(false, dest)) {
-					validTree = false;
-				}
-			}
-			if (validTree) {
-				visited.remove(curState);
-				isValidTree.put(curState, true);
-				return true;
-			}
-		}
-		visited.remove(curState);
-		isValidTree.put(curState, false);
-		toDelete.add(curState);
-		return false;
-	}
-
-	public boolean getResult() {
-		Set<STATE> allStates = mtree.getStates();
-		Set<STATE> initialStates = mtree.getInitStates();
-		Set<STATE> finalStates = mtree.getFinalStates();
-		Map<STATE, Collection<BuchiTreeAutomatonRule<LETTER, STATE>>> sourceMap = mtree.getSourceMap();
-		Map<STATE, Collection<BuchiTreeAutomatonRule<LETTER, STATE>>> childMap = mtree.getChildMap();
-		while (!allStates.isEmpty()) {
-			boolean allValidTree = true;
-			for (STATE next : allStates) {
-				if (!eliminateNode(true, next)) {
-					allValidTree = false;
+	private void initializeGoodTransitions() {
+		Set<BuchiTreeAutomatonRule<LETTER, STATE>> allRules = mtree.getRules();
+		for (BuchiTreeAutomatonRule<LETTER, STATE> rule : allRules) {
+			List<STATE> dests = rule.getDest();
+			boolean isGoodTransition = true;
+			for (STATE dest : dests) {
+				if (!finalStates.contains(dest)) {
+					isGoodTransition = false;
 					break;
 				}
 			}
-			if (allValidTree) {
-				if (initialStates.isEmpty()) {
-					return true;
-				}
-				return false;
-			} else {
-				for (STATE next : toDelete) {
-					if (initialStates.contains(next)) {
-						initialStates.remove(next);
-					}
-					if (finalStates.contains(next)) {
-						finalStates.remove(next);
-					}
-					if (sourceMap.containsKey(next)) {
-						sourceMap.remove(next);
-					}
-					if (allStates.contains(next)) {
-						allStates.remove(next);
-					}
-					if (childMap.containsKey(next)) {
-						for (BuchiTreeAutomatonRule<LETTER, STATE> rule : childMap.get(next)) {
-							STATE source = rule.getSource();
-							if (sourceMap.containsKey(source) && sourceMap.get(source).contains(rule)) {
-								sourceMap.get(source).remove(rule);
+			if (isGoodTransition)
+				goodTransitions.add(rule);
+		}
+	}
+
+	private void findAllGoodStates() {
+		while (!goodTransitions.isEmpty()) {
+			BuchiTreeAutomatonRule<LETTER, STATE> nextRule = goodTransitions.pop();
+			STATE src = nextRule.getSource();
+			if (!goodStates.contains(src)) {
+				goodStates.add(src);
+				Collection<BuchiTreeAutomatonRule<LETTER, STATE>> ruleToSrc = mtree.getChildMap().get(src);
+				if (ruleToSrc != null && !finalStates.contains(src)) {
+					for (BuchiTreeAutomatonRule<LETTER, STATE> rule : ruleToSrc) {
+						boolean isGoodTransition = true;
+						for (STATE dest : rule.getDest()) {
+							if (!finalStates.contains(dest) && !goodStates.contains(dest)) {
+								isGoodTransition = false;
+								break;
 							}
+						}
+						if (isGoodTransition) {
+							goodTransitions.add(rule);
 						}
 					}
 				}
-				toDelete.clear();
-				visited.clear();
-				isValidTree.clear();
+			}
+		}
+	}
+
+	/**
+	 * Return true if a final state is removed and false otherwise.
+	 * 
+	 * @return
+	 */
+	private boolean removeNotGoodStates() {
+		boolean finalStateRemoved = false;
+		Iterator<STATE> iterInitStates = mtree.getInitStates().iterator();
+		while (iterInitStates.hasNext()) {
+			STATE nextInitState = iterInitStates.next();
+			if (!goodStates.contains(nextInitState)) {
+				iterInitStates.remove();
+				removeState(nextInitState);
+			}
+		}
+
+		Iterator<STATE> iterFinalStates = finalStates.iterator();
+		while (iterFinalStates.hasNext()) {
+			STATE nextFinalState = iterFinalStates.next();
+			if (!goodStates.contains(nextFinalState)) {
+				iterFinalStates.remove();
+				removeState(nextFinalState);
+				finalStateRemoved = true;
+			}
+		}
+		return finalStateRemoved;
+	}
+
+	private void removeState(STATE s) {
+		Collection<BuchiTreeAutomatonRule<LETTER, STATE>> rulesToS = mtree.getChildMap().get(s);
+		if (rulesToS != null) {
+			for (BuchiTreeAutomatonRule<LETTER, STATE> rule : rulesToS) {
+				for (STATE dest : rule.getDest()) {
+					if (!dest.equals(s) && mtree.getChildMap().get(dest) != null
+							&& mtree.getChildMap().get(dest).contains(rule)) {
+						mtree.getChildMap().get(dest).remove(rule);
+					}
+				}
+				if (mtree.getSourceMap().get(rule.getSource()) != null
+						&& mtree.getSourceMap().get(rule.getSource()).contains(rule)) {
+					mtree.getSourceMap().get(rule.getSource()).remove(rule);
+				}
+			}
+			mtree.getChildMap().remove(s);
+		}
+
+		Collection<BuchiTreeAutomatonRule<LETTER, STATE>> rulesFromS = mtree.getSourceMap().get(s);
+		if (rulesFromS != null) {
+			for (BuchiTreeAutomatonRule<LETTER, STATE> rule : rulesFromS) {
+				for (STATE dest : rule.getDest()) {
+					if (!dest.equals(s) && mtree.getChildMap().get(dest) != null
+							&& mtree.getChildMap().get(dest).contains(rule)) {
+						mtree.getChildMap().get(dest).remove(rule);
+					}
+				}
+			}
+			mtree.getSourceMap().remove(s);
+		}
+	}
+
+	public boolean computeResult() {
+		initializeGoodTransitions();
+		findAllGoodStates();
+		while (!goodStates.isEmpty()) {
+			if (!removeNotGoodStates()) {
+				if (mtree.getInitStates().isEmpty()) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				if (mtree.getInitStates().isEmpty()) {
+					return true;
+				}
+				goodStates.clear();
+				goodTransitions.clear();
+				initializeGoodTransitions();
+				findAllGoodStates();
 			}
 		}
 		return true;
