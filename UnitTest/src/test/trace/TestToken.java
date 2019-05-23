eedd074f1829917ac668e5f391edf4d024b46de1
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-05-23 14:21:52 -0400

    added the unit test files for Token and TokenFactory.

---
+++ UnitTest/src/test/trace/TestToken.java
@@ -0,0 +1,39 @@
+package test.trace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import usra.trace.Token;
+
+class TestToken {
+
+	@BeforeAll
+	static void init() {
+	}
+
+	@Test
+	void testVariable() {
+		int i = (int) (Math.random() * 1000);
+		int j = (int) (Math.random() * 1000);
+		Token t1 = new Token(Token.VARIABLE, i);
+		Token t2 = new Token(Token.VARIABLE, j);
+		assertEquals(t1.getType(), Token.VARIABLE);
+		assertEquals(t1.getValue(), i);
+		assertEquals(t2.getType(), Token.VARIABLE);
+		assertEquals(t2.getValue(), j);
+	}
+
+	@Test
+	void testNumerical() {
+		int i = (int) (Math.random() * 1000);
+		int j = (int) (Math.random() * 1000);
+		Token t1 = new Token(Token.NUMERICAL, i);
+		Token t2 = new Token(Token.NUMERICAL, j);
+		assertEquals(t1.getType(), Token.NUMERICAL);
+		assertEquals(t1.getValue(), i);
+		assertEquals(t2.getType(), Token.NUMERICAL);
+		assertEquals(t2.getValue(), j);
+	}
+}
---
+++ UnitTest/src/test/trace/TestTokenFactory.java
@@ -0,0 +1,41 @@
+package test.trace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import usra.trace.Token;
+import usra.trace.TokenFactory;
+
+public class TestTokenFactory {
+
+	@BeforeAll
+	static void init() {
+	}
+
+	@Test
+	void testVariable() {
+		int n = (int) (Math.random() * 10000);
+		Token[] tokens = new Token[n];
+		TokenFactory factory = new TokenFactory();
+		for (int i = 0; i < n; i++) {
+			tokens[i] = factory.createVariable();
+		}
+		for (int i = 0; i < n; i++) {
+			assertEquals(tokens[i].getType(), Token.VARIABLE);
+			assertEquals(tokens[i].getValue(), i + 1);
+		}
+	}
+
+	@Test
+	void testNumerical() {
+		TokenFactory factory = new TokenFactory();
+		for (int i = 0; i < 10000; i++) {
+			int j = (int) (Math.random() * 600);
+			Token next = factory.createNumerical(j);
+			assertEquals(next.getType(), Token.NUMERICAL);
+			assertEquals(next.getValue(), j);
+		}
+	}
+}
