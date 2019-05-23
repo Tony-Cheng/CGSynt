c05d3da7b7152ab6745340b804a5066d451b82fa
benweisz <ben.weisz@mail.utoronto.ca>
2019-05-23 16:20:38 -0400

    made minor change to Trace.java

--- Test/src/TestTraceOp.java
+++ Test/src/TestTraceOp.java
@@ -1,14 +1,20 @@
 import usra.trace.Formula;
+import usra.trace.FormulaFactory;
 import usra.trace.Token;
 import usra.trace.TokenFactory;
+import usra.trace.Trace;
 
 public class TestTraceOp {
 	public static void main(String args[]) {
 		 TokenFactory tf = new TokenFactory();
 		 
 		 Token x = tf.createVariable();
-		 Token zero = tf.createNumerical(0);
+		
+		 Formula f1 = FormulaFactory.assign(x, 0);
+		 Formula f2 = FormulaFactory.assume(x, tf.createNumerical(2));
 		 
-		 Formula f1 = new Formula();
+		 Trace t = new Trace();
+		 t.addFormula(f1);
+		 t.addFormula(f2);
 	}
 }
