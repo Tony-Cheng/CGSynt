5ae4da15e2ffc56142b78681f3f28afa849ca79e
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-05-24 11:51:42 -0400

    tested generating trace and static assignments from a DFA.

--- UnitTest/src/test/dfa/to/trace/GenerateTracesFromDFA.java
+++ UnitTest/src/test/dfa/to/trace/GenerateTracesFromDFA.java
@@ -10,11 +10,15 @@ import org.junit.jupiter.api.Test;
 import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
 import usra.trace.Formula;
 import usra.trace.Token;
 import usra.trace.TokenFactory;
 import usra.trace.Trace;
+import usra.trace.operations.TraceScriptToInterpolants;
+import usra.trace.operations.TraceToSMTFormula;
 
 public class GenerateTracesFromDFA {
 
@@ -33,8 +37,19 @@ public class GenerateTracesFromDFA {
 		Set<Trace> traces = DFAToTraceBFS.bfs(nwa, 5);
 		assertEquals(traces.size(), 2);
 		for (Trace trace : traces) {
+			TraceToSMTFormula op = new TraceToSMTFormula(trace);
+			Script s = op.getResult();
+
+			Term[] terms = s.getAssertions();
+						
 			System.out.println(trace);
+
+			for (Term term : terms) {
+				System.out.println(term);
+			}
+			System.out.println();
 		}
+
 	}
 
 }
