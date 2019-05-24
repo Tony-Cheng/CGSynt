c8d5db183fefd8a9b1bf393307bdba3b40c490df
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-05-24 15:01:38 -0400

    added tests for computing interpolants from program traces.

---
+++ UnitTest/src/test/trace/TestInterpolants.java
@@ -0,0 +1,101 @@
+package test.trace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import usra.trace.FormulaFactory;
+import usra.trace.Token;
+import usra.trace.TokenFactory;
+import usra.trace.Trace;
+import usra.trace.operations.TraceScriptToInterpolants;
+import usra.trace.operations.TraceToSMTFormula;
+
+public class TestInterpolants {
+	@BeforeAll
+	static void init() {
+	}
+
+	@Test
+	void computeSimpleTrace() {
+		// x:=0 y:=0 x++ x==-1
+		Trace trace = new Trace();
+		TokenFactory factory = new TokenFactory();
+		Token x = factory.createVariable();
+		Token y = factory.createVariable();
+		trace.addFormulas(FormulaFactory.assign(x, 0), FormulaFactory.assign(y, 0),
+				FormulaFactory.assign(x, FormulaFactory.addition(x, 1)), FormulaFactory.assume(x, -1));
+		TraceToSMTFormula traceToFormula = new TraceToSMTFormula(trace);
+		Script result = traceToFormula.getResult();
+		Term[] terms = result.getAssertions();
+		assertEquals(terms.length, 4);
+		System.out.println("Trace: x:=0 y:=0 x++ x==-1");
+		for (Term term : terms) {
+			System.out.println(term);
+		}
+		TraceScriptToInterpolants interpol = new TraceScriptToInterpolants(result, 4);
+		Term[] interpolTerms = interpol.getResult();
+		for (Term term : interpolTerms) {
+			System.out.println(term);
+		}
+		System.out.println();
+	}
+
+	@Test
+	void computeComplexSingleVariableTrace() {
+		// x:=10 x:=x*10 x:=x+10 x>10 x>=20 x==0
+		Trace trace = new Trace();
+		TokenFactory factory = new TokenFactory();
+		Token x = factory.createVariable();
+		trace.addFormulas(FormulaFactory.assign(x, 10), FormulaFactory.assign(x, FormulaFactory.mult(x, 10)),
+				FormulaFactory.assign(x, FormulaFactory.addition(x, 10)), FormulaFactory.gt(x, 10),
+				FormulaFactory.geq(x, 20), FormulaFactory.assume(x, 0));
+		TraceToSMTFormula traceToFormula = new TraceToSMTFormula(trace);
+		Script result = traceToFormula.getResult();
+		Term[] terms = result.getAssertions();
+		assertEquals(terms.length, 6);
+		System.out.println("Trace: x:=10 x:=x*10 x:=x+10 x>10 x>=20 x==0");
+		for (Term term : terms) {
+			System.out.println(term);
+		}
+		TraceScriptToInterpolants interpol = new TraceScriptToInterpolants(result, 6);
+		Term[] interpolTerms = interpol.getResult();
+		for (Term term : interpolTerms) {
+			System.out.println(term);
+		}
+		System.out.println();
+	}
+
+	@Test
+	void computeComplexMultiVariable() {
+		// x:=0 y:=0 z:=0 x++ y++ z++ x++ x==z
+		Trace trace = new Trace();
+		TokenFactory factory = new TokenFactory();
+		Token x = factory.createVariable();
+		Token y = factory.createVariable();
+		Token z = factory.createVariable();
+		trace.addFormulas(FormulaFactory.assign(x, 0), FormulaFactory.assign(y, 0), FormulaFactory.assign(z, 0),
+				FormulaFactory.assign(x, FormulaFactory.addition(x, 1)),
+				FormulaFactory.assign(y, FormulaFactory.addition(y, 1)),
+				FormulaFactory.assign(z, FormulaFactory.addition(z, 1)),
+				FormulaFactory.assign(x, FormulaFactory.addition(x, 1)), FormulaFactory.assume(x, z));
+		TraceToSMTFormula traceToFormula = new TraceToSMTFormula(trace);
+		Script result = traceToFormula.getResult();
+		Term[] terms = result.getAssertions();
+		assertEquals(terms.length, 8);
+		System.out.println("Trace: x:=0 y:=0 z:=0 x++ y++ z++ x++ x==z");
+		for (Term term : terms) {
+			System.out.println(term);
+		}
+		TraceScriptToInterpolants interpol = new TraceScriptToInterpolants(result, 8);
+		Term[] interpolTerms = interpol.getResult();
+		for (Term term : interpolTerms) {
+			System.out.println(term);
+		}
+		System.out.println();
+	}
+
+}
