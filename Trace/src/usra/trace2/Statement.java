af329e8ffcff98119b02cb1198157e69aa7f7436
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-05-26 14:59:13 -0400

    added a statement abstract class.

---
+++ Trace/src/usra/trace2/Statement.java
@@ -0,0 +1,19 @@
+package usra.trace2;
+
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+
+public abstract class Statement {
+	
+	private final boolean isAssignment;
+
+	public Statement(boolean isAssignment) {
+		this.isAssignment = isAssignment;
+	}
+	
+	public boolean isAssignment() {
+		return isAssignment;
+	}
+	
+	public abstract Term generateTerm(int id);
+	
+}
