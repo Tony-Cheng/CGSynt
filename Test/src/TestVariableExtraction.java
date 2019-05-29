3dfadae3bdabe12b826daad737141fb7ca0e0bd2
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-05-29 16:32:37 -0400

    Testing varaible extraction

--- Test/src/TestVariableExtraction.java
+++ Test/src/TestVariableExtraction.java
@@ -1,16 +1,22 @@
+import java.util.Set;
+
+import de.uni_freiburg.informatik.ultimate.logic.Logics;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Sort;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.DefaultLogger;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.smtlib2.SMTInterpol;
 
 public class TestVariableExtraction {
 	public static void defineVars(Script script) {
 		script.declareFun("x", new Sort[0], script.sort("Int"));
+		script.declareFun("y", new Sort[0], script.sort("Int"));
 	}
 	
 	public static void main(String args[]) {
 		Script script = new SMTInterpol(new DefaultLogger());
+		script.setLogic(Logics.QF_UFLIA);
 		defineVars(script);
 		
 		Term x = script.term("x");
@@ -18,7 +24,6 @@ public class TestVariableExtraction {
 		
 		Term t = script.term("*", x, y);
 		
-		
-		
+		System.out.println(t);
 	}
 }
