169c4ce88d45f156e79541d281edfa6a58dfee25
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-05-30 12:42:19 -0400

    Starting tests with basic predicate

--- Test/src/TestPredicates.java
+++ Test/src/TestPredicates.java
@@ -1,8 +1,19 @@
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicate;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicateFactory;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
 
 public class TestPredicates {
-
 	public static void main(String[] args) {
-
+		IUltimateServiceProvider mock = UltimateMocks.createUltimateServiceProviderMock();
+		Script script = new SMTInterpol(new DefaultLogger());
+		
+		ManagedScript ms = new ManagedScript(mock);
+		
+		
+		BasicPredicateFactory pf = new BasicPredicateFactory(mock, );
+		
+		BasicPredicate b = new BasicPredicate(0, new String[0], null, null, null);
 	}
-
 }
