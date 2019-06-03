83368f385f40336542531a7622b0b382ac2fbeef
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-06-03 14:56:08 -0400

    Set up CfgBuilder

--- Test/src/TTC/BoogieParsingDebug.java
+++ Test/src/TTC/BoogieParsingDebug.java
@@ -3,6 +3,7 @@ package TTC;
 import java.io.File;
 import java.io.IOException;
 
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Unit;
 import de.uni_freiburg.informatik.ultimate.boogie.parser.BoogieParser;
 import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger.LogLevel;
@@ -13,15 +14,14 @@ public class BoogieParsingDebug {
 	public BoogieParsingDebug() throws IOException {
 		File[] files = new File[1];
 		files[0] = new File("res/code.bpl"); 
-		// The parser requires an absolute path so you need to change this
 		
 		UltimateServiceProviderMock mock = (UltimateServiceProviderMock) UltimateMocks.createUltimateServiceProviderMock(LogLevel.DEBUG);
 		
 		BoogieParser bp = new BoogieParser();
 		bp.setServices(mock);
 		
-		IElement ast = bp.parseAST(files);
-		System.out.println("Parsed!");
+		Unit ast = (Unit)bp.parseAST(files);
+		System.out.println("");
 	}
 	
 	public static void main(String args[]) throws IOException {
--- Test/src/TTC/MainTTCTest.java
+++ Test/src/TTC/MainTTCTest.java
@@ -1,5 +1,54 @@
 package TTC;
 
-public class MainTTCTest {
+import java.io.File;
+import java.io.IOException;
 
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Unit;
+import de.uni_freiburg.informatik.ultimate.boogie.parser.BoogieParser;
+import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.CfgBuilder;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger.LogLevel;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
+
+public class MainTTCTest {
+	public MainTTCTest() {
+		IUltimateServiceProvider mock = UltimateMocks.createUltimateServiceProviderMock(LogLevel.DEBUG);
+		BoogieParser bp = new BoogieParser();
+		bp.setServices(mock);
+		
+		Unit unit = (Unit)parseBoogie(bp, "res/code.bpl");
+		
+		CfgBuilder cfgBuilder = makeCfgBuilder(unit, mock);
+	}
+	
+	private CfgBuilder makeCfgBuilder(Unit unit, IUltimateServiceProvider service) {
+		try {
+			return new CfgBuilder(unit, service);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	private IElement parseBoogie(BoogieParser parser, String path) {
+		File file = new File(path);
+		File[] files = new File[]{file};
+		
+		IElement ast = null;
+		
+		try {
+			ast = parser.parseAST(files);
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+			System.exit(1);
+		}
+		
+		return ast;
+	}
+	
+	public static void main(String[] args) {
+		new MainTTCTest();
+	}
 }
