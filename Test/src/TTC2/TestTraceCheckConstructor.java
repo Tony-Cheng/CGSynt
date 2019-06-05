525de4a401b945935f62e4e33b156a99156c3cdf
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-05 16:49:13 -0400

    constructed trace check in TestTraceCheckConstructor. Currently there are errors.

--- Test/src/TTC2/TestTraceCheckConstructor.java
+++ Test/src/TTC2/TestTraceCheckConstructor.java
@@ -7,23 +7,49 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
+import de.uni_freiburg.informatik.ultimate.boogie.type.BoogieType;
+import de.uni_freiburg.informatik.ultimate.core.model.models.IBoogieType;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
+import de.uni_freiburg.informatik.ultimate.logic.Logics;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Sort;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieOldVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.CfgSmtToolkit;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.ConcurrencyInformation;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.DefaultIcfgSymbolTable;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.ModifiableGlobalsTable;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.SmtSymbols;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.ThreadInstance;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IAction;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfgForkTransitionThreadCurrent;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfgJoinTransitionThreadCurrent;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdgeFactory;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocation;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.TransFormulaBuilder;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.ILocalProgramVar;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramNonOldVar;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramVar;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils.SimplificationTechnique;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils.XnfConversionTechnique;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicateFactory;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.PredicateUnifier;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.tracecheck.ITraceCheckPreferences.AssertCodeBlockOrder;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.preferences.TAPreferences;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.TraceCheck;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.DefaultLogger;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.smtlib2.SMTInterpol;
+import de.uni_freiburg.informatik.ultimate.test.mocks.ConsoleLogger;
 import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.SerialProvider;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
@@ -32,11 +58,31 @@ public class TestTraceCheckConstructor {
 
 	public static void main(String[] args) {
 		IUltimateServiceProvider service = UltimateMocks.createUltimateServiceProviderMock();
+		ILogger logger = new ConsoleLogger();
 		ManagedScript managedScript = new ManagedScript(service, new SMTInterpol(new DefaultLogger()));
+		managedScript.getScript().setLogic(Logics.QF_LIA);
+
 		DefaultIcfgSymbolTable symbolTable = new DefaultIcfgSymbolTable();
-		TAPreferences taPref = new TAPreferences(service);
-		ModifiableGlobalsTable modifiableGlobalsTable = new ModifiableGlobalsTable(new HashRelation<>());
+		managedScript.getScript().declareFun("var", new Sort[0], managedScript.getScript().sort("Int"));
+		managedScript.getScript().declareFun("var'", new Sort[0], managedScript.getScript().sort("Int"));
+		BoogieOldVar var1 = new BoogieOldVar("var", (IBoogieType) BoogieType.TYPE_INT,
+				managedScript.getScript().variable("var", managedScript.getScript().sort("Int")),
+				(ApplicationTerm) managedScript.getScript().term("var"),
+				(ApplicationTerm) managedScript.getScript().term("var'"));
+		BoogieNonOldVar var2 = new BoogieNonOldVar("var", (IBoogieType) BoogieType.TYPE_INT,
+				managedScript.getScript().variable("var", managedScript.getScript().sort("Int")),
+				(ApplicationTerm) managedScript.getScript().term("var"),
+				(ApplicationTerm) managedScript.getScript().term("var'"), var1);
+		symbolTable.add(var2);
+		// TAPreferences taPref = new TAPreferences(service);
+
+		String procedure1 = "procedure1";
+		HashRelation<String, IProgramNonOldVar> mProc2Globals = new HashRelation<>();
+		mProc2Globals.addPair(procedure1, var2);
+		ModifiableGlobalsTable modifiableGlobalsTable = new ModifiableGlobalsTable(mProc2Globals);
 		Set<String> procedures = new HashSet<>();
+		procedures.add(procedure1);
+
 		Map<String, List<ILocalProgramVar>> inParams = new HashMap<>();
 		Map<String, List<ILocalProgramVar>> outParams = new HashMap<>();
 		IcfgEdgeFactory icfgEdgeFactory = new IcfgEdgeFactory(new SerialProvider());
@@ -46,7 +92,24 @@ public class TestTraceCheckConstructor {
 		SmtSymbols smtSymbols = new SmtSymbols(managedScript.getScript());
 		CfgSmtToolkit toolkit = new CfgSmtToolkit(modifiableGlobalsTable, managedScript, symbolTable, procedures,
 				inParams, outParams, icfgEdgeFactory, concurInfo, smtSymbols);
-		
+		SortedMap<Integer, IPredicate> pendingContexts = new TreeMap<>();
+
+		NestedWord<IAction> trace = new NestedWord<>();
+		TransFormulaBuilder formulaBuilder = new TransFormulaBuilder(null, null, false, null, false, null, false);
+		Term one = managedScript.getScript().numeral("1");
+		formulaBuilder.addAuxVar((TermVariable) one);
+		formulaBuilder.addInVar(var1, var1.getTermVariable());
+		formulaBuilder.addOutVar(var2, var2.getTermVariable());
+		formulaBuilder.setFormula(managedScript.term("=", "var1", one));
+
+		BasicPredicateFactory predicateFactory = new BasicPredicateFactory(service, managedScript, symbolTable,
+				SimplificationTechnique.NONE, XnfConversionTechnique.BDD_BASED);
+		PredicateUnifier pUnifer = new PredicateUnifier(logger, service, managedScript, predicateFactory, symbolTable,
+				SimplificationTechnique.NONE, XnfConversionTechnique.BDD_BASED);
+
+		TraceCheck<IAction> traceCheck = new TraceCheck<>(pUnifer.getTruePredicate(), pUnifer.getFalsePredicate(),
+				pendingContexts, trace, service, toolkit, AssertCodeBlockOrder.NOT_INCREMENTALLY, false, true);
+		traceCheck.isCorrect();
 
 	}
 
