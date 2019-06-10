443870489204c2e2319b1a1376d5c516401b9b1f
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-10 10:55:39 -0400

    added hashCode and equals method for LTAIntersectState.

--- Automata-Extension/src/cgsynt/tree/buchi/LTAIntersectState.java
+++ Automata-Extension/src/cgsynt/tree/buchi/LTAIntersectState.java
@@ -14,5 +14,34 @@ public class LTAIntersectState<STATE> {
 		this.state1 = state1;
 		this.state2 = state2;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((state1 == null) ? 0 : state1.hashCode());
+		result = prime * result + ((state2 == null) ? 0 : state2.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LTAIntersectState other = (LTAIntersectState) obj;
+		if (state1 == null) {
+			if (other.state1 != null)
+				return false;
+		} else if (!state1.equals(other.state1))
+			return false;
+		if (state2 == null) {
+			if (other.state2 != null)
+				return false;
+		} else if (!state2.equals(other.state2))
+			return false;
+		return true;
+	}
 
 }
