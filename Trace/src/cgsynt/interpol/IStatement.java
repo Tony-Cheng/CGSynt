9be47cda4a08b1a25aba908ce1c9c5ce3234d3d8
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-10 16:33:13 -0400

    implemented a subclass for IStatement called ScriptAssignmentStatement.

--- Trace/src/cgsynt/interpol/IStatement.java
+++ Trace/src/cgsynt/interpol/IStatement.java
@@ -3,7 +3,25 @@ package cgsynt.interpol;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IAction;
 
+/**
+ * A programming statement in a trace.
+ *
+ */
 public interface IStatement {
 
+	/**
+	 * Return a trace that only contains this statement.
+	 * 
+	 * @return
+	 */
 	public NestedWord<IAction> getTrace();
+
+	/**
+	 * Return a formula that represents this statement.
+	 * 
+	 * @param prevProcedure
+	 * @param nextProcedure
+	 * @return
+	 */
+	public IAction getFormula(String prevProcedure, String nextProcedure);
 }
---
+++ Trace/src/cgsynt/interpol/ScriptAssignmentStatement.java
@@ -0,0 +1,70 @@
+package cgsynt.interpol;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.DefaultIcfgSymbolTable;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.BasicInternalAction;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IAction;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.TransFormulaBuilder;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.UnmodifiableTransFormula;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramVar;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
+
+public class ScriptAssignmentStatement implements IStatement {
+
+	private BoogieNonOldVar lhs;
+	private Term rhs;
+	private DefaultIcfgSymbolTable symbolTable;
+	private ManagedScript managedScript;
+
+	/**
+	 * Initialize this ScriptStatement object.
+	 * 
+	 * @param lhs
+	 *            the left hand side of the statement.
+	 * @param rhs
+	 *            the right hand side of the statement.
+	 */
+	public ScriptAssignmentStatement(BoogieNonOldVar lhs, Term rhs, DefaultIcfgSymbolTable symbolTable,
+			ManagedScript managedScript) {
+		this.lhs = lhs;
+		this.rhs = rhs;
+		this.symbolTable = symbolTable;
+		this.managedScript = managedScript;
+	}
+
+	@Override
+	public NestedWord<IAction> getTrace() {
+		List<IProgramVar> lhs = new ArrayList<>();
+		List<Term> rhs = new ArrayList<>();
+		lhs.add(this.lhs);
+		rhs.add(this.rhs);
+		UnmodifiableTransFormula formula = TransFormulaBuilder.constructAssignment(lhs, rhs, symbolTable,
+				managedScript);
+		BasicInternalAction basicAction = new BasicInternalAction(null, null, formula);
+		IAction[] word = new IAction[1];
+		int[] nestingRelation = new int[1];
+		word[0] = basicAction;
+		nestingRelation[0] = NestedWord.INTERNAL_POSITION;
+		NestedWord<IAction> trace = new NestedWord<>(word, nestingRelation);
+		return trace;
+	}
+
+	@Override
+	public IAction getFormula(String prevProcedure, String nextProcedure) {
+		List<IProgramVar> lhs = new ArrayList<>();
+		List<Term> rhs = new ArrayList<>();
+		lhs.add(this.lhs);
+		rhs.add(this.rhs);
+		UnmodifiableTransFormula formula = TransFormulaBuilder.constructAssignment(lhs, rhs, symbolTable,
+				managedScript);
+		;
+		return new BasicInternalAction(prevProcedure, nextProcedure, formula);
+	}
+
+}
