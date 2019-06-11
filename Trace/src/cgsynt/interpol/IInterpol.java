78c75ef60dfce855275e3c1beb79aa21c7d5d759
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-11 11:06:58 -0400

    Update IInterpol.java

--- Trace/src/cgsynt/interpol/IInterpol.java
+++ Trace/src/cgsynt/interpol/IInterpol.java
@@ -1,6 +1,12 @@
 package cgsynt.interpol;
 
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
+
 public interface IInterpol {
 
 	public boolean checkSat();
+	
+	public IPredicate[] computeInterpolants(List<IStatement> statements);
 }
