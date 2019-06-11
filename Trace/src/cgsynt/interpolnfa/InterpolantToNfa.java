4e8eb732ab9c687fb8e3e5ed1559de23f4cb4b79
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-11 10:45:45 -0400

    moved the class GlobalVariables.

--- Trace/src/cgsynt/interpolnfa/InterpolantToNfa.java
+++ Trace/src/cgsynt/interpolnfa/InterpolantToNfa.java
@@ -6,7 +6,7 @@ import cgsynt.interpol.IStatement;
 import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.VpAlphabet;
-import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;s
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
 import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
 
