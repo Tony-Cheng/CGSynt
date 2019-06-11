ebf84d8b6a62cdc74eb3c57e8bae6ed9ce24bfc1
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-11 13:21:37 -0400

    added assumption statements.

--- Trace/src/cgsynt/interpol/ScriptAssumptionStatement.java
+++ Trace/src/cgsynt/interpol/ScriptAssumptionStatement.java
@@ -4,37 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
-import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.DefaultIcfgSymbolTable;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.BasicInternalAction;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IAction;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.TransFormulaBuilder;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.UnmodifiableTransFormula;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 
-public class ScriptAssignmentStatement implements IStatement {
+public class ScriptAssumptionStatement implements IStatement {
 
 	private BoogieNonOldVar lhs;
 	private Term rhs;
 	private DefaultIcfgSymbolTable symbolTable;
 	private ManagedScript managedScript;
+	String type;
 
-	/**
-	 * Initialize this ScriptStatement object.
-	 * 
-	 * @param lhs
-	 *            the left hand side of the statement.
-	 * @param rhs
-	 *            the right hand side of the statement.
-	 */
-	public ScriptAssignmentStatement(BoogieNonOldVar lhs, Term rhs) {
-		this.lhs = lhs;
-		this.rhs = rhs;
-		this.symbolTable = TraceGlobalVariables.getGlobalVariables().getVariableFactory().getSymbolTable();
-		this.managedScript = TraceGlobalVariables.getGlobalVariables().getManagedScript();
+	public ScriptAssumptionStatement(BoogieNonOldVar lhs, Term rhs, String type) {
+		lhs = this.lhs;
+		rhs = this.rhs;
+		this.type = type;
+		symbolTable = TraceGlobalVariables.getGlobalVariables().getVariableFactory().getSymbolTable();
+		managedScript = TraceGlobalVariables.getGlobalVariables().getManagedScript();
+
 	}
 
 	@Override
@@ -43,8 +36,8 @@ public class ScriptAssignmentStatement implements IStatement {
 		List<Term> rhs = new ArrayList<>();
 		lhs.add(this.lhs);
 		rhs.add(this.rhs);
-		UnmodifiableTransFormula formula = TransFormulaBuilder.constructAssignment(lhs, rhs, symbolTable,
-				managedScript);
+		UnmodifiableTransFormula formula = ExtendedTransFormulaBuilder.constructAssumption(lhs, rhs, symbolTable,
+				managedScript, type);
 		BasicInternalAction basicAction = new BasicInternalAction(null, null, formula);
 		IAction[] word = new IAction[1];
 		int[] nestingRelation = new int[1];
@@ -60,8 +53,8 @@ public class ScriptAssignmentStatement implements IStatement {
 		List<Term> rhs = new ArrayList<>();
 		lhs.add(this.lhs);
 		rhs.add(this.rhs);
-		UnmodifiableTransFormula formula = TransFormulaBuilder.constructAssignment(lhs, rhs, symbolTable,
-				managedScript);
+		UnmodifiableTransFormula formula = ExtendedTransFormulaBuilder.constructAssumption(lhs, rhs, symbolTable,
+				managedScript, type);
 		return new BasicInternalAction(null, null, formula);
 	}
 
