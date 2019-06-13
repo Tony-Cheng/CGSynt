497bd52ba0dff621ebd35968775861eeffd2e8b6
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-06-13 17:09:36 -0400

    Added test example verification

--- CGSynt/src/cgsynt/loop/CegarRunner.java
+++ CGSynt/src/cgsynt/loop/CegarRunner.java
@@ -1,14 +1,40 @@
 package cgsynt.loop;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import CGSynt.Verification.MainVerificationLoop;
+import cgsynt.interpol.IStatement;
+import cgsynt.interpol.ScriptAssignmentStatement;
+import cgsynt.interpol.TraceGlobalVariables;
+import cgsynt.interpol.VariableFactory;
+import cgsynt.tree.buchi.BuchiTreeAutomaton;
+import cgsynt.tree.buchi.BuchiTreeAutomatonRule;
+import cgsynt.tree.buchi.lta.RankedBool;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
+
 public class CegarRunner {
-	CegarRunner(){
-		ILoop loop = new VerificationLoop(100);
+	CegarRunner() throws Exception{
+		BuchiTreeAutomaton<RankedBool, String> aut = new BuchiTreeAutomaton<>(1);
+		aut.addFinalState("s1");
 		
-		loop.start();
-		System.out.println(loop.getStatus());
+		VariableFactory vf = TraceGlobalVariables.getGlobalVariables().getVariableFactory();
+		Script script = TraceGlobalVariables.getGlobalVariables().getManagedScript().getScript();
+		BoogieNonOldVar x = vf.constructVariable("x", vf.INT);
+		
+		IStatement xpp = new ScriptAssignmentStatement(x, script.term("+", x.getTerm(), script.numeral("1")));
+		List<IStatement> letters = new ArrayList<>();
+		letters.add(xpp);
+		
+		List<String> dest = new ArrayList<>();
+		dest.add("s1");
+		aut.addRule(new BuchiTreeAutomatonRule<>(RankedBool.TRUE, "s1", dest));
+		
+		MainVerificationLoop loop = new MainVerificationLoop(aut, letters);
 	}
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		new CegarRunner();
 	}
 }
