a840f56f01b461ab3f45401164c53807a5865284
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-14 14:05:13 -0400

    added an unit test for the main verification loop.

--- UnitTest/src/test/verification/loop/TestMainVerificationLoop1.java
+++ UnitTest/src/test/verification/loop/TestMainVerificationLoop1.java
@@ -1,7 +1,11 @@
-package cgsynt.loop;
+package test.verification.loop;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 import cgsynt.Verification.MainVerificationLoop;
 import cgsynt.interpol.IStatement;
@@ -14,30 +18,41 @@ import cgsynt.tree.buchi.lta.RankedBool;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
 
-public class CegarRunner {
-	CegarRunner() throws Exception{
+public class TestMainVerificationLoop1 {
+
+	@Test
+	void test1() throws Exception {
+		// No pre and post conditions.
+		// Only a single statement (x++).
 		BuchiTreeAutomaton<RankedBool, String> aut = new BuchiTreeAutomaton<>(1);
-		aut.addFinalState("s1");
 		aut.addInitState("s1");
-		
+		aut.addState("s1");
+		aut.addState("s2");
+		aut.addState("s3");
+		aut.setAllStatesFinal();
 		VariableFactory vf = TraceGlobalVariables.getGlobalVariables().getVariableFactory();
+		BoogieNonOldVar x = vf.constructVariable("x", VariableFactory.INT);
 		Script script = TraceGlobalVariables.getGlobalVariables().getManagedScript().getScript();
-		BoogieNonOldVar x = vf.constructVariable("x", vf.INT);
-		
+
 		IStatement xpp = new ScriptAssignmentStatement(x, script.term("+", x.getTerm(), script.numeral("1")));
 		List<IStatement> letters = new ArrayList<>();
 		letters.add(xpp);
-		
-		List<String> dest = new ArrayList<>();
-		dest.add("s1");
-		aut.addRule(new BuchiTreeAutomatonRule<>(RankedBool.TRUE, "s1", dest));
-		
+
+		List<String> dest1 = new ArrayList<>();
+		dest1.add("s2");
+		aut.addRule(new BuchiTreeAutomatonRule<>(RankedBool.FALSE, "s1", dest1));
+
+		List<String> dest2 = new ArrayList<>();
+		dest2.add("s3");
+		aut.addRule(new BuchiTreeAutomatonRule<>(RankedBool.FALSE, "s2", dest2));
+		aut.addRule(new BuchiTreeAutomatonRule<>(RankedBool.TRUE, "s2", dest2));
+
+		List<String> dest3 = new ArrayList<>();
+		dest3.add("s3");
+		aut.addRule(new BuchiTreeAutomatonRule<>(RankedBool.FALSE, "s3", dest3));
+
 		MainVerificationLoop loop = new MainVerificationLoop(aut, letters, new ArrayList<>(), new ArrayList<>());
 		loop.computeMainLoop();
-		System.out.println(loop.isCorrect());
-	}
-
-	public static void main(String args[]) throws Exception {
-		new CegarRunner();
+		assertTrue(loop.isCorrect());
 	}
 }
