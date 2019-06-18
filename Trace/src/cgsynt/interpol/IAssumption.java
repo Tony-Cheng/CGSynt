7ca9c3599c205f8e3efe4a2e5857abb1e22db5a4
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-18 12:11:36 -0400

    added transitions that loop back to the true predicate in the main verification loop and added javadoc for IAssumption.

--- Trace/src/cgsynt/interpol/IAssumption.java
+++ Trace/src/cgsynt/interpol/IAssumption.java
@@ -1,6 +1,18 @@
 package cgsynt.interpol;
 
+/**
+ * The interface for an assumption statement.
+ *
+ */
 public interface IAssumption extends IStatement {
+	/**
+	 * Negate the assumption.
+	 */
 	public void negate();
+	
+	/**
+	 * Return whether the statement is negated or not.
+	 * @return
+	 */
 	public boolean isNegated();
 }
