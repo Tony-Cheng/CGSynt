5db68c077e0dec679b86ed9048f718f2e8673ff2
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-25 09:55:52 -0400

    fixed a bug in the program automaton construction operation.

--- Automata-Extension/src/cgsynt/tree/buchi/operations/ProgramAutomatonConstructon.java
+++ Automata-Extension/src/cgsynt/tree/buchi/operations/ProgramAutomatonConstructon.java
@@ -40,6 +40,8 @@ public class ProgramAutomatonConstructon {
 		computeStates();
 		computeRightStateEdges();
 		computeBottomStateEdges();
+		computeLeftStateEdges();
+		resultComputed = true;
 	}
 
 	private void computeAssignmentsAndAssumptions() {
