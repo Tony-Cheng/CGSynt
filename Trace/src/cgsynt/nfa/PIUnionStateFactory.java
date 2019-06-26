20a0ec29648da5d88ac4fce887f6e28c5974b213
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-06-26 16:16:12 -0400

    Added Union State factory

---
+++ Trace/src/cgsynt/nfa/PIUnionStateFactory.java
@@ -0,0 +1,26 @@
+package cgsynt.nfa;
+
+import cgsynt.interpol.TraceToInterpolants;
+import de.uni_freiburg.informatik.ultimate.automata.statefactory.IUnionStateFactory;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicateFactory;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
+
+public class PIUnionStateFactory implements IUnionStateFactory<IPredicate>{
+	private static BasicPredicateFactory mPf = TraceToInterpolants.getTraceToInterpolants().getPredicateFactory();
+	
+	@Override
+	public IPredicate createEmptyStackState() {
+		return  mPf.newDebugPredicate("EmptyStackState");
+	}
+
+	@Override
+	public IPredicate createSinkStateContent() {
+		return mPf.newDebugPredicate("SinkStatePredicate");
+	}
+
+	@Override
+	public IPredicate union(IPredicate state1, IPredicate state2) {
+		return mPf.and(state1, state2);
+	}
+
+}
