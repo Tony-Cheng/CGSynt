fcb4436b82bd793f74cdc569b7c508ff80743981
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-06-26 10:29:14 -0400

    added the class Pair in program autmaton constructor.

--- Automata-Extension/src/cgsynt/tree/buchi/operations/ProgramAutomatonConstruction.java
+++ Automata-Extension/src/cgsynt/tree/buchi/operations/ProgramAutomatonConstruction.java
@@ -1,5 +1,6 @@
 package cgsynt.tree.buchi.operations;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +12,7 @@ import cgsynt.interpol.IStatement;
 import cgsynt.tree.buchi.BuchiTreeAutomaton;
 import cgsynt.tree.buchi.BuchiTreeAutomatonRule;
 import cgsynt.tree.buchi.lta.RankedBool;
-import javafx.util.Pair;
+import javafx.beans.NamedArg;
 
 public class ProgramAutomatonConstruction {
 	private Set<IStatement> statements;
@@ -170,4 +171,60 @@ public class ProgramAutomatonConstruction {
 		return alphabet;
 	}
 
+}
+
+class Pair<K, V> {
+	public Pair(K key, V value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "Pair [key=" + key + ", value=" + value + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair other = (Pair) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+	public K getKey() {
+		return key;
+	}
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public V getValue() {
+		return value;
+	}
+	public void setValue(V value) {
+		this.value = value;
+	}
+	private K key;
+	private V value;
+	
 }
