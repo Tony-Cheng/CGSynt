fb427a35a42964b6a1b26214a74923c87fb420c0
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-06-27 12:17:52 -0400

    Added Minimization State Factory

---
+++ Trace/src/cgsynt/nfa/MinimizeStateFactory.java
@@ -0,0 +1,25 @@
+package cgsynt.nfa;
+
+import java.util.Collection;
+
+import cgsynt.interpol.TraceToInterpolants;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.operations.minimization.IMinimizationStateFactory;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicateFactory;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
+
+public class MinimizeStateFactory implements IMinimizationStateFactory<IPredicate>{
+
+	private static BasicPredicateFactory pf = TraceToInterpolants.getTraceToInterpolants().getPredicateFactory();
+	private static int count = 0;
+	
+	@Override
+	public IPredicate merge(Collection<IPredicate> states) {
+		return pf.newDebugPredicate("Simplified: " + count++);
+	}
+
+	@Override
+	public IPredicate createEmptyStackState() {
+		return pf.newDebugPredicate("EmptyState:MinimizeStateFactory");
+	}
+	
+}
--- Trace/src/cgsynt/nfa/PIUnionStateFactory.java
+++ Trace/src/cgsynt/nfa/PIUnionStateFactory.java
@@ -1,21 +1,44 @@
 package cgsynt.nfa;
 
+import cgsynt.interpol.TraceGlobalVariables;
 import cgsynt.interpol.TraceToInterpolants;
+import cgsynt.interpol.VariableFactory;
 import de.uni_freiburg.informatik.ultimate.automata.statefactory.IUnionStateFactory;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicateFactory;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
 
 public class PIUnionStateFactory implements IUnionStateFactory<IPredicate>{
-	private static BasicPredicateFactory mPf = TraceToInterpolants.getTraceToInterpolants().getPredicateFactory();
+	private BasicPredicateFactory mPf = TraceToInterpolants.getTraceToInterpolants().getPredicateFactory();
+	private Script script;
+	private static BoogieNonOldVar sink = null, empty = null;
+	private VariableFactory vf;
+	
+	public PIUnionStateFactory() {
+		this.script = TraceGlobalVariables.getGlobalVariables().getManagedScript().getScript();
+		this.vf = TraceGlobalVariables.getGlobalVariables().getVariableFactory();
+		
+		if (sink == null) {
+			try {
+				PIUnionStateFactory.sink = vf.constructVariable("dontusethisvariablename:sink", VariableFactory.INT);
+				PIUnionStateFactory.empty = vf.constructVariable("dontusethisvariablename:empty", VariableFactory.INT);
+			} catch (Exception e) {
+				System.err.println("Could not create sink/empty state placeholders!");
+				System.exit(1);
+			}
+		}
+	}
 	
 	@Override
 	public IPredicate createEmptyStackState() {
-		return  mPf.newDebugPredicate("EmptyStackState:PIUnionStateFactory");
+		return  mPf.newPredicate(this.script.term("=", empty.getTerm(), script.numeral("0")));
 	}
 
 	@Override
 	public IPredicate createSinkStateContent() {
-		return mPf.newDebugPredicate("SinkStatePredicate:PIUnionStateFactory");
+		
+		return mPf.newPredicate(this.script.term("=", sink.getTerm(), script.numeral("0")));
 	}
 
 	@Override
