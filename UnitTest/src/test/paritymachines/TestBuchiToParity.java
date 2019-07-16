ca6dbe37fdccc48ba60e5525675caeb47fdde75a
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-07-16 14:22:27 -0400

    Verified Tests for BA to PA

--- UnitTest/src/test/paritymachines/TestBuchiToParity.java
+++ UnitTest/src/test/paritymachines/TestBuchiToParity.java
@@ -1,11 +1,10 @@
 package test.paritymachines;
 
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 
 import cgsynt.dfa.parity.ParityAutomaton;
 import cgsynt.dfa.parity.operations.BuchiToParity;
+import cgsynt.tree.parity.ParityState;
 import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
@@ -21,11 +20,32 @@ public class TestBuchiToParity {
 		
 		NestedWordAutomaton<Character, String> buchi = FsaFactory.fiveStateDFA(library);
 		
-		BuchiToParity<Character, String> convert = new BuchiToParity<>(buchi);
-		ParityAutomaton<Character, String> parity = convert.getResult();
+		BuchiToParity<Character, String> convert = new BuchiToParity<>(buchi, library);
+		ParityAutomaton<Character, ParityState<String>> parity = convert.getResult();
 		
-		Map<String, Integer> colouringFunction = parity.getColouringFunction();
+		for (ParityState<String> state : parity.getStates()) {
+			if (parity.isFinal(state))
+				assert state.getRank() == 0;
+			else
+				assert state.getRank() == 1;
+		}
+	}
+	
+	@Test 
+	public void test2() {
+		IUltimateServiceProvider service = UltimateMocks.createUltimateServiceProviderMock();
+		AutomataLibraryServices library = new AutomataLibraryServices(service);
 		
-		System.out.println(colouringFunction);
+		NestedWordAutomaton<Character, String> buchi = FsaFactory.oneStateDfa(library);
+		
+		BuchiToParity<Character, String> convert = new BuchiToParity<>(buchi, library);
+		ParityAutomaton<Character, ParityState<String>> parity = convert.getResult();
+		
+		for (ParityState<String> state : parity.getStates()) {
+			if (parity.isFinal(state))
+				assert state.getRank() == 0;
+			else
+				assert state.getRank() == 1;
+		}
 	}
 }
