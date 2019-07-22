77fa7c2bf4b1386970f296025b9085a443a46748
Ben Weisz <ben.weisz@mail.utoronto.ca>
2019-07-22 16:47:53 -0400

    Started variable input for specification parser

--- CGSynt/src/cgsynt/core/Core.java
+++ CGSynt/src/cgsynt/core/Core.java
@@ -1,48 +1,46 @@
 package cgsynt.core;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import cgsynt.interpol.IStatement;
-import cgsynt.interpol.ScriptAssignmentStatement;
-import cgsynt.interpol.TraceGlobalVariables;
-import cgsynt.interpol.VariableFactory;
-import cgsynt.synthesis.SynthesisLoop;
-import de.uni_freiburg.informatik.ultimate.logic.Script;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicateFactory;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
-
 public class Core {
 	public Core(String[] args) throws Exception {
 		Map<String, List<String>> argsMap = parseArgs(args);
-
 		
-//		// i++; i++
-//		TraceGlobalVariables globalVars = new TraceGlobalVariables();
-//		VariableFactory vf = globalVars.getVariableFactory();
-//		Script script = globalVars.getManagedScript().getScript();
-//		BoogieNonOldVar i = vf.constructVariable("i", VariableFactory.INT);
-//
-//		BasicPredicateFactory predicateFactory = globalVars.getPredicateFactory();
-//
-//		IStatement ipp = new ScriptAssignmentStatement(i, script.term("+", i.getTerm(), script.numeral("1")),
-//				globalVars.getManagedScript(), vf.getSymbolTable());
-//		IStatement imm = new ScriptAssignmentStatement(i, script.term("-", i.getTerm(), script.numeral("1")),
-//				globalVars.getManagedScript(), vf.getSymbolTable());
-//
-//		IPredicate preconditions = predicateFactory.newPredicate(script.term("=", i.getTerm(), script.numeral("0")));
-//		IPredicate postconditions = predicateFactory.newPredicate(script.term("=", i.getTerm(), script.numeral("2")));
-//		List<IStatement> transitionAlphabet = new ArrayList<>();
-//		transitionAlphabet.add(ipp);
-//		transitionAlphabet.add(imm);
-//		SynthesisLoop synthesis = new SynthesisLoop(transitionAlphabet, preconditions, postconditions, globalVars);
-//		synthesis.computeMainLoop();
-//		System.out.println("Test 1");
-//		System.out.println(synthesis.isCorrect());
-//		synthesis.printProgram();
+		if (argsMap.containsKey("-f")){
+			List<String> paths = argsMap.get("-f");
+			
+			for (String path : paths) {
+				BufferedReader reader = null;
+				try {
+					reader = new BufferedReader(new FileReader(path));
+				} catch(IOException ioe) {
+					System.out.println("ERROR: Couldn't read file " + path);
+					System.exit(1);
+				}
+				
+				ArrayList<String> lines = new ArrayList<>();
+				String line;
+				while ((line = reader.readLine()) != null) {
+					lines.add(line);
+				}
+				
+				InputFileParser parser = new InputFileParser();
+				Specification spec = parser.parseFile(lines);
+				//SynthesisLoop synthesis = new SynthesisLoop(spec);
+				//synthesis.computeMainLoop();
+				//System.out.println(synthesis.isCorrect());
+				//synthesis.printProgram();
+			}
+		}
+		else {
+			System.err.println("ERROR: Please specify the input files with the -f flag");
+		}
 	}
 	
 	public Map<String, List<String>> parseArgs(String args[]){
---
+++ CGSynt/src/cgsynt/core/InputFileParser.java
@@ -0,0 +1,101 @@
+package cgsynt.core;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import cgsynt.interpol.TraceGlobalVariables;
+import cgsynt.interpol.VariableFactory;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.BoogieNonOldVar;
+
+public class InputFileParser {	
+	private String[] mVariableTypes = {"int", "bool", "array"};
+	
+	public Specification parseFile(ArrayList<String> lines) throws Exception {
+		TraceGlobalVariables globalVars = new TraceGlobalVariables();
+		VariableFactory vf = globalVars.getVariableFactory();
+		Script script = globalVars.getManagedScript().getScript();
+		
+		ArrayList<BoogieNonOldVar> variables = new ArrayList<BoogieNonOldVar>();
+		Map<String, Integer> mVariableMap = new HashMap<>();
+		
+		int linePointer = 1;
+		
+		// Check that the file starts with Vars marker
+		if (!lines.get(0).equals("Vars"))
+			throw new ParseException("The specification file must begin with \"Vars\""); 
+		
+		while (variableDeclarationLine(lines.get(linePointer))) {
+			String[] tokens = lines.get(linePointer).split(" ");
+			if (tokens.length < 2)
+				throw new ParseException("Line" + linePointer + " must declare both variable type and variable name"); 
+			else if (tokens.length > 2)
+				throw new ParseException("Line" + linePointer + " must only declare variable type and variable name"); 
+			
+			int type = stringToVariableInt(tokens[0]);
+			variables.add(vf.constructVariable(tokens[1], type));
+			mVariableMap.put(tokens[1], linePointer - 1);
+			
+			linePointer++;
+		}
+		
+		
+		// Create Specification file using the parsed data.
+		Specification spec = new Specification(null, null, null, null);
+		
+		return spec;
+	}
+	
+	private boolean variableDeclarationLine(String line) {
+		for (int i = 0; i < mVariableTypes.length; i++) {
+			if (line.startsWith(mVariableTypes[i]))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	private int stringToVariableInt(String type) {
+		if (type.equals("int"))
+			return VariableFactory.INT;
+		else if (type.equals("bool"))
+			return VariableFactory.BOOL;
+		else if (type.equals("array"))
+			return VariableFactory.INT_ARR;
+		return -1;
+	}
+}
+
+class ParseException extends Exception{
+	private static final long serialVersionUID = 6420159566994853936L;
+
+	public ParseException(String message) {
+		System.err.println("PARSE ERROR: " + message);
+		System.exit(1);
+	}
+}
+
+//// i++; i++
+//TraceGlobalVariables globalVars = new TraceGlobalVariables();
+//VariableFactory vf = globalVars.getVariableFactory();
+//Script script = globalVars.getManagedScript().getScript();
+//BoogieNonOldVar i = vf.constructVariable("i", VariableFactory.INT);
+//
+//BasicPredicateFactory predicateFactory = globalVars.getPredicateFactory();
+//
+//IStatement ipp = new ScriptAssignmentStatement(i, script.term("+", i.getTerm(), script.numeral("1")),
+//		globalVars.getManagedScript(), vf.getSymbolTable());
+//IStatement imm = new ScriptAssignmentStatement(i, script.term("-", i.getTerm(), script.numeral("1")),
+//		globalVars.getManagedScript(), vf.getSymbolTable());
+//
+//IPredicate preconditions = predicateFactory.newPredicate(script.term("=", i.getTerm(), script.numeral("0")));
+//IPredicate postconditions = predicateFactory.newPredicate(script.term("=", i.getTerm(), script.numeral("2")));
+//List<IStatement> transitionAlphabet = new ArrayList<>();
+//transitionAlphabet.add(ipp);
+//transitionAlphabet.add(imm);
+//SynthesisLoop synthesis = new SynthesisLoop(transitionAlphabet, preconditions, postconditions, globalVars);
+//synthesis.computeMainLoop();
+//System.out.println("Test 1");
+//System.out.println(synthesis.isCorrect());
+//synthesis.printProgram();
