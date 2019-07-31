b7e621204bf9f9861d9139e9233009448b914bb0
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-07-31 12:00:29 -0400

    generated the hashcode and the equal methods for the nfa intersected state.

--- Automata-Extension/src/cgsynt/nfa/operations/NFAIntersectedState.java
+++ Automata-Extension/src/cgsynt/nfa/operations/NFAIntersectedState.java
@@ -11,4 +11,38 @@ public class NFAIntersectedState<STATE1, STATE2> {
 		this.state2 = state2;
 		this.mode = mode;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + mode;
+		result = prime * result + ((state1 == null) ? 0 : state1.hashCode());
+		result = prime * result + ((state2 == null) ? 0 : state2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NFAIntersectedState other = (NFAIntersectedState) obj;
+		if (mode != other.mode)
+			return false;
+		if (state1 == null) {
+			if (other.state1 != null)
+				return false;
+		} else if (!state1.equals(other.state1))
+			return false;
+		if (state2 == null) {
+			if (other.state2 != null)
+				return false;
+		} else if (!state2.equals(other.state2))
+			return false;
+		return true;
+	}
 }
