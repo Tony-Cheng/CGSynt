ac8d0bbf168f523269f64a5ed156b625832b2976
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-08-19 22:40:53 -0400

    implemented an alternating parity automaton.

---
+++ Automata-Extension/src/cgsynt/alternating/parity/AlternatingParityAutomaton.java
@@ -0,0 +1,60 @@
+package cgsynt.alternating.parity;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import cgsynt.tree.parity.IParityState;
+import cgsynt.tree.parity.ParityTreeAutomaton;
+import cgsynt.tree.parity.ParityTreeAutomatonRule;
+import de.uni_freiburg.informatik.ultimate.automata.tree.IRankedLetter;
+
+public class AlternatingParityAutomaton<LETTER extends IRankedLetter, STATE extends IParityState> {
+	private Set<STATE> states;
+	private Set<STATE> initialStates;
+	private Set<AlternatingParityAutomatonRule<LETTER, STATE>> rules;
+	private Map<STATE, Set<AlternatingParityAutomatonRule<LETTER, STATE>>> sourceMap;
+	private Map<LETTER, Set<AlternatingParityAutomatonRule<LETTER, STATE>>> letterMap;
+	private Map<STATE, Map<LETTER, AlternatingParityAutomatonRule<LETTER, STATE>>> parentMap;
+
+	public AlternatingParityAutomaton(ParityTreeAutomaton<LETTER, STATE> treeAut) {
+		this.initialStates = new HashSet<>();
+		this.states = new HashSet<>();
+		this.initialStates.addAll(treeAut.getInitStates());
+		this.states.addAll(treeAut.getStates());
+		this.computeRules(treeAut);
+	}
+
+	private void computeRules(ParityTreeAutomaton<LETTER, STATE> treeAut) {
+		this.rules = new HashSet<>();
+		this.sourceMap = new HashMap<>();
+		this.parentMap = new HashMap<>();
+		for (STATE state : treeAut.getStates()) {
+			sourceMap.put(state, new HashSet<>());
+			parentMap.put(state, new HashMap<>());
+			for (LETTER letter : treeAut.getAlphabet()) {
+				if (!letterMap.containsKey(letter)) {
+					letterMap.put(letter, new HashSet<>());
+				}
+				if (!treeAut.getParentsMap().get(state).containsKey(letter)) {
+					continue;
+				}
+				Set<Set<STATE>> dests = new HashSet<>();
+				for (ParityTreeAutomatonRule<LETTER, STATE> rule : treeAut.getRules()) {
+					Set<STATE> dest = new HashSet<>();
+					dest.addAll(rule.getDest());
+					dests.add(dest);
+				}
+				AlternatingParityAutomatonRule<LETTER, STATE> rule = new AlternatingParityAutomatonRule<>(state, letter,
+						dests);
+				rules.add(rule);
+				sourceMap.get(state).add(rule);
+				parentMap.get(state).put(letter, rule);
+				letterMap.get(letter).add(rule);
+			}
+		}
+	}
+	
+	
+}
