9289a56f282d90d919630477c17696e8e9f53651
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-08-23 15:03:48 -0400

    fixed the bugs in the buchi parity intersection.

--- Automata-Extension/src/cgsynt/tree/buchi/parity/BuchiParityIntersectState.java
+++ Automata-Extension/src/cgsynt/tree/buchi/parity/BuchiParityIntersectState.java
@@ -1,15 +1,14 @@
 package cgsynt.tree.buchi.parity;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 import cgsynt.tree.parity.IExtendedParityState;
 import cgsynt.tree.parity.IParityState;
 
-public class BuchiParityIntersectState<STATE1, STATE2 extends IParityState> implements IExtendedParityState {	
+public class BuchiParityIntersectState<STATE1, STATE2 extends IParityState> implements IExtendedParityState {
 	private static int LABEL = 0;
-	
+
 	private final BuchiParityPair<STATE1, STATE2> mState;
 	private final int mK;
 	private final Map<BuchiParityPair<STATE1, STATE2>, Integer> visitedStates;
@@ -22,14 +21,14 @@ public class BuchiParityIntersectState<STATE1, STATE2 extends IParityState> impl
 		this.visitedStates = new HashMap<>();
 		this.mStateLabel = LABEL++;
 	}
-	
+
 	private BuchiParityIntersectState(BuchiParityPair<STATE1, STATE2> state, int k) {
 		super();
 		this.mState = state;
 		this.mK = k;
 		this.visitedStates = new HashMap<>();
 	}
-	
+
 	public BuchiParityIntersectState<STATE1, STATE2> nextState(BuchiParityPair<STATE1, STATE2> state, int k) {
 		BuchiParityIntersectState<STATE1, STATE2> nextState = new BuchiParityIntersectState<>(state,
 				Math.max(k, this.mK));
@@ -60,8 +59,6 @@ public class BuchiParityIntersectState<STATE1, STATE2 extends IParityState> impl
 		int result = 1;
 		result = prime * result + mK;
 		result = prime * result + ((mState == null) ? 0 : mState.hashCode());
-		result = prime * result + ((visitedStates == null) ? 0 : visitedStates.hashCode());
-		result = prime * result + mStateLabel;
 		return result;
 	}
 
@@ -73,7 +70,7 @@ public class BuchiParityIntersectState<STATE1, STATE2 extends IParityState> impl
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		BuchiParityIntersectState other = (BuchiParityIntersectState) obj;
+		BuchiParityIntersectState<STATE1, STATE2> other = (BuchiParityIntersectState<STATE1, STATE2>) obj;
 		if (mK != other.mK)
 			return false;
 		if (mState == null) {
@@ -84,16 +81,26 @@ public class BuchiParityIntersectState<STATE1, STATE2 extends IParityState> impl
 		if (visitedStates == null) {
 			if (other.visitedStates != null)
 				return false;
-		} else if (!visitedStates.equals(other.visitedStates))
+		}
+		if (!other.visitedStates.keySet().containsAll(visitedStates.keySet())) {
 			return false;
+		}
+		if (other.visitedStates.keySet().size() != visitedStates.keySet().size()) {
+			return false;
+		}
+		for (BuchiParityPair<STATE1, STATE2> pair : visitedStates.keySet()) {
+			if (other.visitedStates.get(pair) != visitedStates.get(pair)) {
+				return false;
+			}
+		}
 		return true;
 	}
 
 	@Override
 	public void setRank(int newRank) {
 		assert mState.getState2() instanceof IExtendedParityState;
-		
-		((IExtendedParityState)mState).setRank(newRank);
+
+		((IExtendedParityState) mState).setRank(newRank);
 	}
 
 	public IParityState makeCopy() {
@@ -102,9 +109,7 @@ public class BuchiParityIntersectState<STATE1, STATE2 extends IParityState> impl
 
 	@Override
 	public String toString() {
-		return "BuchiParityIntersectState [state=" + mState + ", k=" + mK + ", \nvisitedStates=" + visitedStates
-				+ "]";
+		return "BuchiParityIntersectState [state=" + mState + ", k=" + mK + ", \nvisitedStates=" + visitedStates + "]";
 	}
-
 
 }
