4a2d2762a006733b37df64b7cfd77b25d2330a43
Yizhe Cheng <yizhe.cheng@mail.utoronto.ca>
2019-08-30 16:53:55 -0400

    created a new project for sample code.

---
+++ SampleCode/src/cgsynt/synthesis/sample/SynthesizeTerminatingProgram.java
@@ -0,0 +1,17 @@
+package cgsynt.synthesis.sample;
+
+public class SynthesizeTerminatingProgram {
+
+	/**
+	 * Try to synthesize the following program:
+	 * while(i < n):
+	 * 	i++
+	 */
+	private static void example1() {
+		
+	}
+	public static void main(String[] args) {
+		example1();
+	}
+
+}
